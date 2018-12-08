2a26efa5dae5d241534e4945e2463ae5bad89977
Alex Martin <atonymartin20@yahoo.com>
2018-12-08 11:44:51 -0600

    Completed index.js file

--- .index.js
+++ .index.js
@@ -1,2 +1,23 @@
 //Imports
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const actionsRouter = require('./routers/actionsRouter.js');
+const projectsRouter = require('./routers/projectsRouter.js');
+
+const server = express();
+const PORT = process.env.PORT || 4500;
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+
+//Listening
+server.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+});
