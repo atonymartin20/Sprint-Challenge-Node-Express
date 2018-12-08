229b8bc0291a9c04f15e164f1517e9e96cfee1f1
Alex Martin <atonymartin20@yahoo.com>
2018-12-08 13:10:39 -0600

    Completed CRUD operations on actionsRouter and projectsRouter

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -35,4 +35,77 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const action = req.body;
+    if(action) {
+        actionsDB.insert(action).then(actionId => {
+            actionsDB.get(actionId.id)
+                .then(action => {
+                    res.status(201).json(action)
+                })
+        })
+
+        .catch(err => {
+            res.status(500)
+                .json({ error: "There was an error adding action project to the database." })
+        })
+    }
+
+    else {
+        res.status(400)
+            .json({ errorMessage: "Please provide the project_id, description, notes, and completed boolean for the action." });
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    actionsDB.remove(id)
+        .then(count => {
+            if(count) {
+                res.json({ message: "Action removed succesfully." });
+            }
+
+            else {
+                res.status(404)
+                    .json({ message: "The action with the specified ID does not exist." });
+            }
+        })
+
+        .catch(err => {
+            res.status(500)
+                .json({ error: "The action could not be removed." });
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const action = req.body;
+    const { id } = req.params;
+    if(action.description) {
+        actionsDB.update(id, action)
+            .then(count => {
+                if(count) {
+                    actionsDB.get(id)
+                        .then(action => {
+                            res.json(action);
+                        });
+                }
+
+                else {
+                    res.status(404)
+                        .json({ message: "The action with the specified ID does not exist. "});
+                }
+            })
+
+        .catch(err => {
+            res.status(500)
+                .json({ error: "The action info could not be modified." });
+        })
+    }
+
+    else {
+        res.status(400)
+            .json({ errorMessage: "Please provide the project_id, description, notes, and completed boolean for the action." });
+    }
+})
+
 module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -53,4 +53,77 @@ router.get('/actions/:projectId', (req, res) => {
                 .json({ error: "Project information could not be retrieved"})
         })
 })
+
+router.post('/', (req, res) => {
+    const project = req.body;
+    if(project) {
+        projectsDB.insert(project).then(projectId => {
+            projectsDB.get(projectId.id)
+                .then(project => {
+                    res.status(201).json(project);
+                });
+        })
+
+        .catch(err => {
+            res.status(500)
+                .json({ error: "There was an error adding project to the database." })
+        })
+    }
+
+    else {
+        res.status(400)
+            .json({ errorMessage: "Please provide the name, desciption, and completed boolean for the project." });
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    projectsDB.remove(id)
+        .then(count => {
+            if(count) {
+                res.json({ message: "Project removed succesfully." });
+            }
+            else {
+                res.status(404)
+                    .json({ message: "The project with the specified ID does not exist. "});
+            }
+        })
+
+        .catch(err => {
+            res.status(500)
+                .json({ error: "The project could not be removed." });
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const project = req.body;
+    const { id } = req.params;
+    if(project.name) {
+        projectsDB.update(id, project)
+            .then(count => {
+                if(count) {
+                    projectsDB.get(id)
+                        .then(project => {
+                            res.json(project);
+                        });
+                }
+
+                else {
+                    res.status(404)
+                        .json({ message: "The project with the specified ID does not exist. "});
+                }
+        })
+
+        .catch(err => {
+            res.status(500)
+                .json({ error: "The project info could not be modified." });
+        })
+    }
+
+    else {
+        res.status(400)
+            .json({ errorMessage: "Please provide the name, desciption, and completed boolean for the project." });
+    }
+})
+
 module.exports = router;
